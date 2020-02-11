70d732de853e52c313dfb077cdc4ed131ae1d8ad
TranHoang1 <tran4080@gmail.com>
2020-02-11 11:39:00 +0700

    MS Image in Gallery

---
+++ MicroService-Image/src/main/java/com/example/demo/model/Image.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+public class Image {
+
+	public Image(Integer id, String title, String url) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.url = url;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	private Integer id;
+	private String title;
+	private String url;
+
+}
